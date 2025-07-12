cdee72773fb1f0c00a30576ddade27bed1871669
JIAXIN123YANG <jiaxin123yang@gmail.com>
2025-07-12 05:21:32 +0200

    fix footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,131 +1,92 @@
 <template>
-  <footer>
-    <div class="bottom">
-      <div class="informations">
-        <div class="number">
-          <b>Number</b>
+  <footer class="bg-[#006a71] border-t-4 border-[#48a6a7] text-white font-['Jost'] text-sm mt-8">
+    <div class="flex flex-col lg:flex-row justify-between flex-wrap gap-8 py-8 px-32">
+
+      <!-- ✅ Menu Links（两列） -->
+
+        <!-- 左列：其他菜单 -->
+        <div class="space-y-2">
+          <div
+              v-for="link in otherLinks"
+              :key="link.title"
+          >
+            <NuxtLink :to="link.path" class="font-semibold hover:text-gray-200">
+              {{ link.title }}
+            </NuxtLink>
+          </div>
+        </div>
+
+        <!-- 右列：Activities + 子菜单 -->
+        <div>
+          <NuxtLink
+              :to="activitiesLink.path"
+              class="font-semibold text-base hover:text-gray-200"
+          >
+            {{ activitiesLink.title }}
+          </NuxtLink>
+          <ul class="mt-2 space-y-1 text-sm ml-1">
+            <li v-for="sublink in activitiesLink.dropdown" :key="sublink.title">
+              <NuxtLink :to="sublink.path" class="hover:text-gray-200">
+                {{ sublink.title }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
+
+
+      <!-- 📌 联系方式 -->
+      <div class="space-y-4 text-sm">
+        <div>
+          <p class="font-semibold">Number</p>
           <p>123-456-7890</p>
         </div>
-        <div class="email">
-          <b>Email</b>
+        <div>
+          <p class="font-semibold">Email</p>
           <p>contact.mail@mail.com</p>
         </div>
-        <div class="address">
-          <b>Headquarters</b>
+        <div>
+          <p class="font-semibold">Headquarters</p>
           <p>Piazza Leonardo da Vinci, 32</p>
         </div>
       </div>
-      <div class="socials">
-        <a href="https://www.instagram.com" target="”_blank”" aria-label="Link to Instagram">
-          <img src="../public/img/contacts/instagram_logo_hover.png" width="512" height="512" alt="Instagram Logo" class="instagram-footer"/>
+
+      <!-- 📌 社交 -->
+      <div class="flex gap-8">
+        <a href="https://www.instagram.com" target="_blank" aria-label="Instagram">
+          <img src="/img/contacts/instagram_logo_hover.png" alt="Instagram" class="w-8 hover:scale-110 transition" />
         </a>
-        <a href="https://www.facebook.com" target="”_blank”" aria-label="Link to Facebook">
-          <img src="../public/img/contacts/facebook_logo_hover.png" width="512" height="512" alt="Facebook Logo" class="facebook-footer"/></a>
-        <a href="https://twitter.com/home" target="”_blank”" aria-label="Link to Twitter">
-          <img src="../public/img/contacts/twitter_logo_hover.png" width="512" height="512" alt="Twitter Logo" class="twitter-footer"/>
+        <a href="https://www.facebook.com" target="_blank" aria-label="Facebook">
+          <img src="/img/contacts/facebook_logo_hover.png" alt="Facebook" class="w-8 hover:scale-110 transition" />
+        </a>
+        <a href="https://twitter.com/home" target="_blank" aria-label="Twitter">
+          <img src="/img/contacts/twitter_logo_hover.png" alt="Twitter" class="w-8 hover:scale-110 transition" />
         </a>
       </div>
     </div>
   </footer>
 </template>
 
-<style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600&display=swap");
+<script setup>
+import { ref, computed } from 'vue'
 
-footer {
-  background-color: #006a71;
-  flex-shrink: 0;
-  border-top: 5px solid #48a6a7;
-  color: #ffffff;
-  font-family: "Jost";
-  font-size: 0.9vw;
-  margin-top: 2%;
-}
+const links = ref([
+  { title: "Home", path: "/" },
+  {
+    title: "Activities",
+    path: "/activities",
+    dropdown: [
+      { title: "Highlights✨", path: "/activities/Highlights" },
+      { title: "Type1", path: "/activities/Type1" },
+      { title: "Type2", path: "/activities/Type2" },
+      { title: "Type3", path: "/activities/Type3" },
+    ]
+  },
+  { title: "Teachers", path: "/teachers" },
+  { title: "About us", path: "/about_us" },
+  { title: "Contact us", path: "/contact_us" },
+])
 
-b {
-  font-size: 1vw;
-}
-
-.socials {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  gap: 2vw;
-}
-
-.instagram-footer, .facebook-footer, .twitter-footer {
-  width: 2.5vw;
-  height: auto;
-  position: relative;
-  transition: opacity 0.3s ease-in-out;
-}
-
-.instagram-footer:hover, .facebook-footer:hover, .twitter-footer:hover {
-  transform: scale(1.1);
-}
-
-.instagram-footer:active, .facebook-footer:active, .twitter-footer:active {
-  transform: scale(1);
-}
-
-.bottom {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-}
-
-.informations {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 10vw;
-  padding-top: 1.5vw;
-  padding-bottom: 1vw;
-}
-
-.contacts {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 3vw;
-}
-
-@media screen and (max-width: 768px) {
-  .bottom {
-    flex-direction: column-reverse;
-    text-align: center;
-    margin-top: 0;
-  }
-
-  .informations {
-    flex-direction: column-reverse;
-    gap: 1vw;
-    padding-bottom: 0;
-  }
-
-  .address, .number, .email {
-    padding: 0;
-  }
-
-  .socials {
-    margin-top: 3vw;
-    gap: 2vh;
-  }
-
-  b {
-    font-size: 2vh;
-  }
-
-  .instagram-footer, .facebook-footer, .twitter-footer {
-    width: 4vh;
-  }
-
-  footer {
-    font-size: 1.9vh;
-  }
-}
-</style>
-<script setup lang="ts">
+// 分离出 Activities 和 其他链接
+const activitiesLink = computed(() => links.value.find(link => link.title === 'Activities'))
+const otherLinks = computed(() => links.value.filter(link => link.title !== 'Activities'))
 </script>
